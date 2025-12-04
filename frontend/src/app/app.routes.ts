098c329c743186ef25d88535287ea845f8bc74a9
Druindarora <druindar.inarora@protonmail.com>
2025-12-04 10:07:11 +0100

    feat: add header and footer components, update app layout
    - Introduced `Header` and `Footer` components for consistent layout.
    - Updated `App` component to include header and footer.
    - Created `Home` and `Portfolio` components with basic structure.
    - Defined routing for home and portfolio pages.
    - Added styles for header, footer, and home components.
    - Implemented tests for new components.
    - Updated package.json to include linting tools.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Portfolio } from './portfolio/portfolio';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+// app.routes.ts (ou app-routing.module.ts selon ta version)
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'portfolio', component: Portfolio },
+//   { path: 'memo-quiz', loadChildren: () => import('./memo-quiz/memo-quiz.routes').then(m => m.routes) },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './core/layout/header/header';
+import { Footer } from './core/layout/footer/footer';
 
 export interface Profile {
   fullName: string;
@@ -11,32 +12,8 @@ export interface Profile {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  styleUrls: ['./app.scss'],
 })
-export class App {
-  profile: Profile | undefined;
-  protected readonly title = signal('Sidequestlab');
-
-  private readonly baseUrl = 'http://localhost:8080/api';
-
-  constructor(private http: HttpClient) {
-    // this.getData();
-  }
-
-  ngOnInit(): void {
-    this.getData(); // Moved to constructor to ensure data is fetched on component initialization
-  }
-
-  getData(): void {
-    this.getProfile().subscribe(data => {
-      console.log(data); // ici tu vois déjà "William from database"
-      this.profile = data;
-    });
-  }
-
-  getProfile() {
-    return this.http.get<Profile>(`${this.baseUrl}/profile/me`);
-  }
-}
+export class App {}
