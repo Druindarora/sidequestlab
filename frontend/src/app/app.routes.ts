1566c01b9a97537a9e53fe3e1e8fa3aee64b3003
Druindarora <druindar.inarora@protonmail.com>
2026-06-10 17:28:02 +0200

    refactor: add public and private app layouts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,14 +5,23 @@ import { Profil } from './public/pages/profil/profil';
 import { memoQuizDemoGuard } from './core/auth/memo-quiz-demo.guard';
 import { memoQuizAuthGuard } from './core/auth/memo-quiz-auth.guard';
 import { DemoMemoquiz } from './memo-quiz/pages/demo-memoquiz/demo-memoquiz';
+import { PrivateLayout } from './private/layout/private-layout/private-layout';
+import { PublicLayout } from './public/layout/public-layout/public-layout';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'profil', component: Profil },
-  { path: 'portfolio', component: Portfolio },
-  { path: 'demo-memoquiz', canMatch: [memoQuizDemoGuard], component: DemoMemoquiz },
+  {
+    path: '',
+    component: PublicLayout,
+    children: [
+      { path: '', component: Home },
+      { path: 'profil', component: Profil },
+      { path: 'portfolio', component: Portfolio },
+      { path: 'demo-memoquiz', canMatch: [memoQuizDemoGuard], component: DemoMemoquiz },
+    ],
+  },
   {
     path: 'memo-quiz',
+    component: PrivateLayout,
     canMatch: [memoQuizAuthGuard],
     loadChildren: () => import('./memo-quiz/memo-quiz.routes').then((m) => m.MEMO_QUIZ_ROUTES),
   },
